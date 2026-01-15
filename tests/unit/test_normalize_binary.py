046c24c28b9e5153b469d22755a7ae8b2138b645
amanbinepal <binepal2003@gmail.com>
2026-01-14 19:15:18 -0800

    added suggestions from LLM (Claude), added log on GitHub issues

--- tests/unit/test_normalize_binary.py
+++ tests/unit/test_normalize_binary.py
@@ -9,6 +9,8 @@ from survey_cleaner.normalize_binary import normalize_binary
         ("T", 1),
         ("t", 1),
         ("Yes", 1),
+        ("yEs", 1),
+        ("YES", 1),
         ("yes", 1),
         ("True", 1),
         ("true", 1),
@@ -32,6 +34,8 @@ def test_normalize_binary_true(true_input_response, true_expected_response):
         ("f", 0),
         ("No", 0),
         ("no", 0),
+        ("NO", 0),
+        ("nO", 0),
         ("False", 0),
         ("false", 0),
         (0, 0)
@@ -53,10 +57,12 @@ def test_normalize_binary_false(false_input_response, false_expected_response):
         (" "),
         (None),
         (-1),
+        (2),
         (" No "),
         (" No"),
         ("No "),
-        ("maybe")
+        ("maybe"),
+        ("Maybe")
     ]
 )
 
